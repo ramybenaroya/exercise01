b7779b390291939c3feaf8a2e57fe501c27ea72c
ramyb <rami@dudamobile.com>
2015-08-05 11:10:55 +0300

    removing libs

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 (function(global, $){
 
-	$.getJSON('game-of-thrones.json', function(data){
-		var template = $('#houses').html();
-		$('body').append(Handlebars.compile(template)(data));
-	});
+	//Your code goes here
 
 })(this, $)
