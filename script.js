843969eba90bce0cb4fbd1ef26a14df0bcd72e25
ramyb <rami@dudamobile.com>
2015-08-18 12:04:01 +0300

    final solution

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 (function(global, $){
 
-	$.getJSON('game-of-thrones.json', function(data){
+	var jsonDataPromise = fetchData();
+
+	function init(){
+		jsonDataPromise.then(render);
+	}
+
+	function fetchData() {
+		return $.getJSON('game-of-thrones.json');
+	}
+
+	function render(data) {
 		var template = $('#houses').html();
 		$('body').append(Handlebars.compile(template)(data));
-	});
+	}
+
+	$(init);
 
 })(this, $)
